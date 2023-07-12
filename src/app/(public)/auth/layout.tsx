161c282a62015765a028592fea51fd3788999db1
Arif Sardar <as2048282@gmail.com>
2023-07-12 16:57:05 +0530

    1.0.2+4

--- src/app/(public)/auth/layout.tsx
+++ src/app/(public)/auth/layout.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import "./global.scss";
+import { Metadata } from 'next';
 
-export default function Layout({ children }: { children: React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             {children}
         </>
     )
 }
+
+/**
+ * Generating meta data for the page
+ */
+type MetaDataProps = {
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "Sign In",
+        description: "Sign in to your account to access all of the features of our website.",
+        openGraph: {
+            type: 'website',
+            title: "Sign In",
+            description: "Sign in to your account to access all of the features of our website."
+        },
+        twitter: {
+            site: '@site',
+            card: 'summary_large_image',
+            title: "Sign In",
+            description: "Sign in to your account to access all of the features of our website."
+        },
+        appleWebApp: {
+            title: "Sign In",
+        },
+    }
+}
+
