8d5cc379e6355b6ddedd0d80d43dddbf7db37953
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-09-19 20:40:17 +0530

    Designed Testimonial Section

--- src/components/showcase/stories/index.tsx
+++ src/components/showcase/stories/index.tsx
@@ -6,7 +6,7 @@ import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import style from './style.module.scss'
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-import { TRACE_OUTPUT_VERSION } from 'next/dist/shared/lib/constants';
+import Image from 'next/image';
 
 export default function Stories({ data: { } }) {
     return (
@@ -30,21 +30,78 @@ export default function Stories({ data: { } }) {
                 modules={[EffectCoverflow, Pagination, Navigation]}
                 className={style.slideContainer}
             >
+                
                 <SwiperSlide className={style.slide}>
-                    <h1>Hello There</h1>
+                    <div className={style.profileInfo}>
+                        <Image className={style.profilePic} width={60} height={60} src={"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpYyUyMGFtbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"}/>
+                        <div className={style.profileName}>
+                            <h2>Akash Srinivasan</h2>
+                            <p>Full Stack Product Developer</p>
+                        </div>
+                    </div>
+
+                    <div className={style.testimonial}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, totam quis adipisci sapiente praesentium voluptas unde temporibus architecto ea eum sequi recusandae vero consequuntur mollitia expedita! Sit perferendis accusamus nesciunt tempora aut, possimus et. Atque doloremque nostrum natus accusantium necessitatibus.
+                    </div>
                 </SwiperSlide>
+
                 <SwiperSlide className={style.slide}>
-                    <h1>Hello There</h1>
+                    <div className={style.profileInfo}>
+                        <Image className={style.profilePic} width={60} height={60} src={"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpYyUyMGFtbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"}/>
+                        <div className={style.profileName}>
+                            <h2>Akash Srinivasan</h2>
+                            <p>Full Stack Product Developer</p>
+                        </div>
+                    </div>
+
+                    <div className={style.testimonial}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia accusantium dolor voluptas soluta sapiente ipsum consequatur officia illum, necessitatibus quisquam iusto neque aliquid? Magnam, illo mollitia.
+                    </div>
                 </SwiperSlide>
+
                 <SwiperSlide className={style.slide}>
-                    <h1>Hello There</h1>
+                    <div className={style.profileInfo}>
+                        <Image className={style.profilePic} width={60} height={60} src={"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpYyUyMGFtbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"}/>
+                        <div className={style.profileName}>
+                            <h2>Akash Srinivasan</h2>
+                            <p>Full Stack Product Developer</p>
+                        </div>
+                    </div>
+
+                    <div className={style.testimonial}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error autem maxime architecto, facilis magni, repudiandae, repellendus distinctio quibusdam magnam qui vero consequuntur eligendi dolor natus. Officia optio reprehenderit doloremque tempore aperiam blanditiis magnam quae! Non laborum quod quam dignissimos quibusdam quis quas dolor suscipit voluptas!
+                    </div>
                 </SwiperSlide>
+
                 <SwiperSlide className={style.slide}>
-                    <h1>Hello There</h1>
+                    <div className={style.profileInfo}>
+                        <Image className={style.profilePic} width={60} height={60} src={"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpYyUyMGFtbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"}/>
+                        <div className={style.profileName}>
+                            <h2>Akash Srinivasan</h2>
+                            <p>Full Stack Product Developer</p>
+                        </div>
+                    </div>
+
+                    <div className={style.testimonial}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit quis facere unde, accusamus aspernatur esse neque eos facilis laudantium ipsa sapiente veritatis sit consequuntur nemo pariatur nobis minus vero numquam dolore aliquid est. Illo, cum.
+                    </div>
                 </SwiperSlide>
+
                 <SwiperSlide className={style.slide}>
-                    <h1>Hello There</h1>
+                    <div className={style.profileInfo}>
+                        <Image className={style.profilePic} width={60} height={60} src={"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpYyUyMGFtbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"}/>
+                        <div className={style.profileName}>
+                            <h2>Akash Srinivasan</h2>
+                            <p>Full Stack Product Developer</p>
+                        </div>
+                    </div>
+
+                    <div className={style.testimonial}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, eveniet?
+                    </div>
                 </SwiperSlide>
+
+                
             </Swiper>
         </div>
     )
